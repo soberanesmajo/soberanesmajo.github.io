2cc73e96ef77c96fcf36d9674677f792476c6d52
soberanesmajo <soberanesmajo@gmail.com>
2018-11-04 23:00:33 -0600

    Updates

---
+++ precache-manifest.db3e9a2f255570ced25183d4cddc08f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "874b8e1d7874925a7beefa596d870f07",
+    "url": "/static/media/stars1.874b8e1d.png"
+  },
+  {
+    "revision": "2463c50671a631ebeee93c7548064529",
+    "url": "/static/media/MajoSoberanes.2463c506.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0daf5613638251c943f",
+    "url": "/static/js/main.f0daf561.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "f0daf5613638251c943f",
+    "url": "/static/css/main.9256ea69.chunk.css"
+  },
+  {
+    "revision": "e923fd6906da5ed889f534f93de43458",
+    "url": "/index.html"
+  }
+];
--- src/components/proyectos/Proyectos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './proyectos.css';
-
-const Proyectos = () =>
-  <div>
-    <h1 id="projects">Proyectos</h1>
-  </div>
-
-export default Proyectos;
