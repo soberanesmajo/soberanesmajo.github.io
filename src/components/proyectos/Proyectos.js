13ab1b3de7033d9d7d6baf569549919a747f86f5
soberanesmajo <soberanesmajo@gmail.com>
2018-11-13 14:54:39 -0600

    agrega tarjetas actualizadas

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -13,20 +13,12 @@ const Proyectos = () =>
           <div className="card">
             <img className="card-img-top" src={Data_dashboard} alt="imagen del proyecto"></img>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="n" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div className="card">
-            <img className="card-img-top" src={Social_network} alt="imagen del proyecto"></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="n" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title">Data Dashboard</h5>
+              <p className="card-text">Proyecto creado para convertir data en información útil y eficaz como herramienta para el seguimiento y control de equipos de trabajo.</p>
+              <div className="btn-center">
+                <a href="https://soberanesmajo.github.io/cdmx-2018-06-bc-core-am-data-dashboard/src/index.html" className=""><button type="button" class="btn btn-outline-light">Demo</button></a>
+                <a href="https://github.com/soberanesmajo/cdmx-2018-06-bc-core-am-data-dashboard" className=""><button type="button" class="btn btn-outline-light">Código</button></a>
+              </div>
             </div>
           </div>
         </div>
@@ -35,13 +27,29 @@ const Proyectos = () =>
           <div className="card">
             <img className="card-img-top" src={SciFi_blog} alt="imagen del proyecto"></img>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="n" className="btn btn-primary">Go somewhere</a>
+              <h5 className="card-title">Sci-fi Blog!</h5>
+              <p className="card-text">Blog de películas de ciencia ficción.</p>
+              <div className="btn-center">
+                <a href="https://soberanesmajo.github.io/cdmx20181-Track-FE-themovieapi/src/index.html" className=""><button type="button" class="btn btn-outline-light">Demo</button></a>
+                <a href="https://github.com/soberanesmajo/cdmx20181-Track-FE-themovieapi" className=""><button type="button" class="btn btn-outline-light">Código</button></a>
+              </div>
             </div>
           </div>
         </div>
+        <div>
 
+          <div className="card">
+            <img className="card-img-top" src={Social_network} alt="imagen del proyecto"></img>
+            <div className="card-body">
+              <h5 className="card-title">Inside Out!</h5>
+              <p className="card-text">Red social especializada en salud mental y emocional para jóvenes.</p>
+              <div className="btn-center">
+                <a href="n" className=""><button type="button" class="btn btn-outline-light">Demo</button></a>
+                <a href="n" className=""><button type="button" class="btn btn-outline-light">Código</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
