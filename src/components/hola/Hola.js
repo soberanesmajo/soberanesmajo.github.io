bbedcebd52189a2305cc700787d3466b7903764b
soberanesmajo <soberanesmajo@gmail.com>
2018-11-29 17:26:58 -0600

    actualiza cards

--- src/components/hola/Hola.js
+++ src/components/hola/Hola.js
@@ -10,11 +10,8 @@ const Hola = () =>
       </section>
       <section className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
         <h1 id="hi">¡Hola!</h1>
-        <p>
-        Soy una desarrolladora web que disfruta de crear a través de la tecnología. Entusiasta del autoaprendizaje y fanática del machine learning. Amante de la pizza, los robots y la ciencia ficción.
-        </p>
-        <p>
-        Me gusta el trabajo en equipo porque me permite aprender a través de personas con diversos perfiles. Practico la metodología ágil y considero la comunicación eficaz lo más importante para la realización de un proyecto.
+        <p id = "text">
+        Soy una desarrolladora web que disfruta de crear a través de la tecnología. Entusiasta del autoaprendizaje y fanática del machine learning. Me gusta el trabajo en equipo porque me permite aprender a través de personas con diversos perfiles. Practico la metodología ágil y considero la comunicación eficaz lo más importante para la realización de un proyecto. Amante de la pizza, los robots y la ciencia ficción.
         </p>
         <div className="btn-cv">
           <a href="https://github.com/soberanesmajo/soberanesmajo.github.io/raw/ramamajo/src/assets/CV.pdf" download="Majo_Soberanes_CV"><button type="button" className="btn btn-outline-light">Descarga CV</button></a>
--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './proyectos.css';
 import Data_dashboard from '../../assets/data.png';
 import Social_network from '../../assets/social_network.png';
-import SciFi_blog from '../../assets/sci-fi_blog.png';
+import cat from '../../assets/gato.png';
 
 const Proyectos = () =>
   <div id="proyectos">
@@ -25,27 +25,27 @@ const Proyectos = () =>
 
         <div>
           <div className="card">
-            <img className="card-img-top" src={SciFi_blog} alt="imagen del proyecto"></img>
+            <img className="card-img-top" src={Social_network} alt="imagen del proyecto"></img>
             <div className="card-body">
-              <h5 className="card-title">Sci-fi Blog!</h5>
-              <p className="card-text">Blog de películas de ciencia ficción.</p>
+              <h5 className="card-title">Inside-Out</h5>
+              <p className="card-text">Red social especializada en salud mental y emocional para jóvenes.</p>
               <div className="btn-center">
-                <a target="_blank" rel="noopener noreferrer" href="https://soberanesmajo.github.io/cdmx20181-Track-FE-themovieapi/src/index.html" className=""><button type="button" className="btn btn-outline-light">Demo</button></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/soberanesmajo/cdmx20181-Track-FE-themovieapi" className=""><button type="button" className="btn btn-outline-light">Código</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://soberanesmajo.github.io/cdmx-2018-06-bc-core-am-social-network/src/index.html" className=""><button type="button" className="btn btn-outline-light">Demo</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/soberanesmajo/cdmx-2018-06-bc-core-am-social-network" className=""><button type="button" className="btn btn-outline-light">Código</button></a>
               </div>
             </div>
           </div>
         </div>
-        <div>
 
+        <div>
           <div className="card">
-            <img className="card-img-top" src={Social_network} alt="imagen del proyecto"></img>
+            <img className="card-img-top" src={cat} alt="imagen del proyecto"></img>
             <div className="card-body">
-              <h5 className="card-title">Inside Out!</h5>
-              <p className="card-text">Red social especializada en salud mental y emocional para jóvenes.</p>
+              <h5 className="card-title">Tic tac toe game!</h5>
+              <p className="card-text">El clásico juego de Gato elaborado con React Native. Disponible para Android y iOS.</p>
               <div className="btn-center">
-                <a target="_blank" rel="noopener noreferrer" href="n" className=""><button type="button" className="btn btn-outline-light">Demo</button></a>
-                <a target="_blank" rel="noopener noreferrer" href="n" className=""><button type="button" className="btn btn-outline-light">Código</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://expo.io/@majosoberanes/native-game" className=""><button type="button" className="btn btn-outline-light">Demo</button></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/soberanesmajo/cdmx-2018-06-bc-core-am-tic-tac-toe-App-Native" className=""><button type="button" className="btn btn-outline-light">Código</button></a>
               </div>
             </div>
           </div>
